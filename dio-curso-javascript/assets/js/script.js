b9c17c2ed308ba2affb4a928047003d501d387ae
Luana Costa <luanacostards@gmail.com>
2022-01-01 14:48:44 -0300

    Criando funções propostas no desafio

--- dio-curso-javascript/assets/js/script.js
+++ dio-curso-javascript/assets/js/script.js
@@ -4,46 +4,41 @@ var decrementBtn = document.getElementById("decrementBtn");
 var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
 
-incrementBtn.addEventListener("click", function increment() {
-  currentNumber++;
-  currentNumberWrapper.innerHTML = currentNumber;
 
-  if (currentNumber < -10) {
+function buttonsAlt(){
+  if (currentNumber <= -10) {
     decrementBtn.style.display = "none";
     incrementBtn.style.display = "block";
-  } else if (currentNumber > 10) {
+  } else if (currentNumber >= 10) {
     incrementBtn.style.display = "none";
     decrementBtn.style.display = "block";
-  } else if (currentNumber >= -10 || currentNumber <= 10) {
+  } else if (currentNumber > -10 || currentNumber < 10) {
     incrementBtn.style.display = "block";
     decrementBtn.style.display = "block";
   }
+}
 
+function negColors(){
   if (currentNumber < 0) {
     currentNumberWrapper.style.color = "red";
   } else {
     currentNumberWrapper.style.color = "black";
   }
+}
+
+
+incrementBtn.addEventListener("click", function increment() {
+  currentNumber++;
+  currentNumberWrapper.innerHTML = currentNumber;
+
+  buttonsAlt();
+  negColors();
 });
 
 decrementBtn.addEventListener("click", function decrement() {
   currentNumber--;
   currentNumberWrapper.innerHTML = currentNumber;
 
-  if (currentNumber < -10) {
-    decrementBtn.style.display = "none";
-    incrementBtn.style.display = "block";
-  } else if (currentNumber > 10) {
-    incrementBtn.style.display = "none";
-    decrementBtn.style.display = "block";
-  } else if (currentNumber >= -10 || currentNumber <= 10) {
-    incrementBtn.style.display = "block";
-    decrementBtn.style.display = "block";
-  }
-
-  if (currentNumber < 0) {
-    currentNumberWrapper.style.color = "red";
-  } else {
-    currentNumberWrapper.style.color = "black";
-  }
+  buttonsAlt();
+  negColors();
 });
