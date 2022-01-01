78003bf3ae709fe05914d8da5e47834357e80ff8
Luana Costa <luanacostards@gmail.com>
2021-12-31 21:53:26 -0300

    Adicionando função aos botões do contador

--- dio-curso-javascript/assets/js/script.js
+++ dio-curso-javascript/assets/js/script.js
@@ -8,9 +8,20 @@ incrementBtn.addEventListener("click", function increment() {
   currentNumber += 1;
   currentNumberWrapper.innerHTML = currentNumber;
 
-  if(currentNumber < 0){
-      currentNumberWrapper.style.color = "red";
-  } else{
+  if (currentNumber < -10) {
+    decrementBtn.style.display = "none";
+    incrementBtn.style.display = "block";
+  } else if (currentNumber > 10) {
+    incrementBtn.style.display = "none";
+    decrementBtn.style.display = "block";
+  } else if (currentNumber >= -10 || currentNumber <= 10) {
+    incrementBtn.style.display = "block";
+    decrementBtn.style.display = "block";
+  }
+
+  if (currentNumber < 0) {
+    currentNumberWrapper.style.color = "red";
+  } else {
     currentNumberWrapper.style.color = "black";
   }
 });
@@ -18,12 +29,21 @@ incrementBtn.addEventListener("click", function increment() {
 decrementBtn.addEventListener("click", function decrement() {
   currentNumber -= 1;
   currentNumberWrapper.innerHTML = currentNumber;
-  
-  if(currentNumber < 0){
-      currentNumberWrapper.style.color = "red";
-  } else{
+
+  if (currentNumber < -10) {
+    decrementBtn.style.display = "none";
+    incrementBtn.style.display = "block";
+  } else if (currentNumber > 10) {
+    incrementBtn.style.display = "none";
+    decrementBtn.style.display = "block";
+  } else if (currentNumber >= -10 || currentNumber <= 10) {
+    incrementBtn.style.display = "block";
+    decrementBtn.style.display = "block";
+  }
+
+  if (currentNumber < 0) {
+    currentNumberWrapper.style.color = "red";
+  } else {
     currentNumberWrapper.style.color = "black";
   }
 });
-
-
