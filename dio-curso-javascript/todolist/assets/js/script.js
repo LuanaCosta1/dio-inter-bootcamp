1d9473f017a54ca0d9739a558fd437db25266ad3
Luana Costa <luanacostards@gmail.com>
2022-01-02 02:08:27 -0300

    Adicionando função de resetar e checked no To-Do List

--- dio-curso-javascript/todolist/assets/js/script.js
+++ dio-curso-javascript/todolist/assets/js/script.js
@@ -1,20 +1,25 @@
 let taskInput = document.getElementById("taskInput");
 let taskAddBtn = document.getElementById("taskAddBtn");
 
-taskAddBtn.addEventListener("click", function createTask() {
-  let todoList = document.getElementById("todo-list");
+if(taskInput.value != ' '){
+  taskAddBtn.addEventListener("click", function createTask() {
+    let todoList = document.getElementById("todo-list");
+    let taskDiv = document.createElement("div");
+    let checkbox = document.createElement("input");
+    let taskContent = document.createElement("label");
+  
+    checkbox.type = "checkbox";
+    checkbox.id = "taskCheckbox";
+  
+    taskContent.setAttribute("for", checkbox);
+    taskContent.className = "taskContent";
+    taskContent.appendChild(document.createTextNode(taskInput.value));
+  
+    taskDiv.className = "taskDiv";
+    taskDiv.append(checkbox, taskContent);
+  
+    todoList.append(taskDiv);
 
-  let taskContent = document.createElement("p");
-  taskContent.className = "taskContent";
-  taskContent.appendChild(document.createTextNode(taskInput.value));
-
-  var checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.className = "taskCheckbox";
-
-  let taskDiv = document.createElement("div");
-  taskDiv.className = "taskDiv"
-  taskDiv.append(checkbox, taskContent);
-
-  todoList.append(taskDiv);
-});
+    taskInput.value = ' ';
+  });
+}
